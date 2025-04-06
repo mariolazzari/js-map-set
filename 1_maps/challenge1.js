76f35df68d1006a91fd57b254469838c0c299550
Mario Lazzari <mario.lazzari@gmail.com>
2025-04-06 13:46:30 +0200

    Done chapter 1 challenge 1

---
+++ 1_maps/challenge1.js
@@ -0,0 +1,14 @@
+const pets = new Map();
+pets.set("Dogs", 1);
+pets.set("Cats", 3);
+pets.set("Horse", 1);
+
+console.log("pets size:", pets.size);
+
+const colors = new Map();
+colors.set("Red", 12);
+colors.set("Blue", 10);
+colors.set("Black", 20);
+colors.set("White", 12);
+
+console.log("colors size:", colors.size);
